caa981a7495329562484a76732ac57f8ea7b572a
J-andrade368 <juliocesarandrade545@gmail.com>
2025-04-02 17:13:27 -0300

    Add files via upload

---
+++ PI = 3.py
@@ -0,0 +1,10 @@
+PI = 3.14
+raio = float(input('Digite o raio do círculo'))
+área = PI*raio**2
+print('A área do círculo é' , format(área))
+
+perímetro = PI**raio 
+print('O perímetro do círculo é' ,format(perímetro))
+
+diâmetro = PI*raio**2
+print('O diâmetro do círculo é' ,format(diâmetro))
